cc1c57e2ef005a303d8111a61518e0fd418297b3
Muhammad Rashid <rashiddaha80@gmail.com>
2024-03-06 23:50:53 +0500

    updates

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
-PROJECT_MAIN_DIR_NAME="django-ec2-autoscaling"
+PROJECT_MAIN_DIR_NAME="django-aws-ec2-autoscaling"
 
 # Copy gunicorn  service file
 sudo cp "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.service" "/etc/systemd/system/gunicorn.service"
